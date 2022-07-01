1d8c608f49740ce26a843cb9eb1c6b80c4a868d5
MangoButtermilch <47949493+MangoButtermilch@users.noreply.github.com>
2022-07-01 22:08:42 +0200

    Create GrassInstancer.cs

---
+++ GrassInstancer.cs
@@ -0,0 +1,147 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct MeshLOD {
+    public Mesh mesh;
+    public float lod;
+    public bool shadows;
+}
+
+public class GrassInstancer : MonoBehaviour {
+
+    private bool _visible;
+    private bool _castShadows;
+    private Mesh _mesh;
+    private Transform _camera;
+    private List<List<Matrix4x4>> _batches = new List<List<Matrix4x4>>();
+
+    [SerializeField] private bool _drawGizmos;
+    [SerializeField][Range(1, 1023)] private int _batchSize = 1000;
+    [SerializeField] private int _instances;
+    [SerializeField] private Vector2 _range;
+    [SerializeField] private Vector3 _scaleMin = Vector3.one;
+    [SerializeField] private Vector3 _scaleMax = Vector3.one;
+    [SerializeField][Range(0f, 1f)] private float _steepness;
+    [SerializeField] private bool _rotateToGroundNormal = false;
+    [SerializeField] private bool _randomYAxisRotation = false;
+    [SerializeField] private float _maxYRotation = 90;
+    [SerializeField] private bool _recieveShadows;
+    [SerializeField] private LayerMask _terrainLayer;
+    [SerializeField] private Material _material;
+    [SerializeField] private MeshLOD[] _meshes;
+    
+    private void Start() {
+        _camera = Camera.main.transform;
+        Initialize();
+        transform.position = new Vector3(transform.position.x, _camera.position.y, transform.position.z);
+    }
+
+    private void Update() {
+        GetMeshFromCameraDistance();
+        RenderBatches();
+    }
+
+    private void OnBecameVisible() {
+        _visible = true;
+    }
+
+    private void OnBecameInvisible() {
+        _visible = false;
+    }
+
+    private void GetMeshFromCameraDistance() {
+        if (!_visible) {
+            _mesh = null;
+            return;
+        }
+        float dist = Mathf.Abs(Vector3.Distance(_camera.position, transform.position));
+        float ratio = dist > 1f ? 1f / Mathf.Clamp(dist, 0.1f, Mathf.Infinity) : dist;
+        for (int i = _meshes.Length - 1; i >= 0; i--) {
+            if (ratio <= _meshes[i].lod) {
+                _mesh = _meshes[i].mesh;
+                _castShadows = _meshes[i].shadows;
+                break;
+            }
+        }
+    }
+
+    private void RenderBatches() {
+        if (_mesh == null) return;
+        for (int i = 0; i < _batches.Count; i++) {
+            Graphics.DrawMeshInstanced(
+                _mesh,
+                 0,
+                _material,
+                _batches[i],
+                null,
+                _castShadows ? UnityEngine.Rendering.ShadowCastingMode.On : UnityEngine.Rendering.ShadowCastingMode.Off,
+                _recieveShadows
+            );
+        }
+    }
+
+    private void OnDrawGizmos() {
+        if(!_drawGizmos) return;
+        _camera = Camera.main.transform;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(_range.x * 2, 5, _range.y * 2));
+
+    }
+
+    private void Initialize() {
+        _batches.Clear();
+        int addedMatricies = 0;
+
+        _batches.Add(new List<Matrix4x4>());
+
+        RaycastHit hit;
+
+        for (int i = 0; i < _instances; i++) {
+            if (addedMatricies < _batchSize && _batches.Count != 0) {
+                Vector3 rayTestPosition = new Vector3(
+                    transform.position.x + Random.Range(-_range.x, _range.x),
+                    transform.position.y + 100,
+                    transform.position.z + Random.Range(-_range.y, _range.y)
+                );
+
+                Ray ray = new Ray(rayTestPosition, Vector3.down);
+                if (!Physics.Raycast(ray, out hit, _terrainLayer)) continue;
+
+                if (hit.transform.tag.Equals("IgnoreRaycast")) continue;  //can be replaced with whatever you want
+
+                float dot = Mathf.Abs(Vector3.Dot(hit.normal, ray.direction));
+                if (dot < _steepness) continue;
+
+                Quaternion tilt = Quaternion.identity;
+                Vector3 eulerIdentiy = Quaternion.ToEulerAngles(Quaternion.identity);
+                eulerIdentiy.x += 90; //can be removed or changed, depends on your mesh
+
+                if (_randomYAxisRotation) eulerIdentiy.y += Random.Range(-_maxYRotation, _maxYRotation);
+
+                if (_rotateToGroundNormal) {
+                    tilt = Quaternion.FromToRotation(Vector3.up, hit.normal) * Quaternion.Euler(eulerIdentiy);
+                } else {
+                    tilt = Quaternion.Euler(eulerIdentiy);
+                }
+
+                Vector3 scale = new Vector3(
+                    Random.Range(_scaleMin.x, _scaleMax.x),
+                    Random.Range(_scaleMin.y, _scaleMax.y),
+                    Random.Range(_scaleMin.z, _scaleMax.z)
+                );
+
+                Vector3 targetPos = hit.point;
+                targetPos.y += scale.y / 2f; //keep or remove, depends on your mesh
+
+                _batches[_batches.Count - 1].Add(Matrix4x4.TRS(targetPos, tilt, scale));
+                addedMatricies++;
+                continue;
+            }
+            _batches.Add(new List<Matrix4x4>());
+            addedMatricies = 0;
+
+        }
+    }
+}
